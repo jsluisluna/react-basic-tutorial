628ec26b348b659e3f1495cb98779dfe94e47029
Jose Luis Luna <jose.luna@globant.com>
2021-04-29 19:28:04 -0500

    Initial commit

---
+++ src/Book.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Book = (props) => {
+  const { img, title, author } = props;
+  // attribute, eventHandler
+  // oncLick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    alert("Hello World");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
+  return (
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
+      <img src={img} alt="" />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
+    </article>
+  );
+};
+
+export default Book;
---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "I love you to the moon and back",
+    author: "Amelia Hepworth",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg",
+    title: "The very hungry caterpillar",
+    author: "Eric Carle",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61TWAvjflGL._AC_UL200_SR200,200_.jpg",
+    title: "Vegetable Simple: A Cookbook",
+    author: "Nigel Parry",
+  },
+];
